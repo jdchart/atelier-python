e4453915ab91ee9e64a5e5f8d7726acf644d1757
Jacob Hart <jacob@uc12825.ufralc.mb-per.uhb.fr>
2023-11-29 09:02:23 +0100

    remove old nakala crap

--- sessions/session-5/scripts/03-nakala-api.py
+++ sessions/session-5/scripts/03-nakala-api.py
@@ -1,54 +1,39 @@
 import requests
+import utils
+import os
 
-class NakalaItem:
-    def __init__(self, url, **kwargs) -> None:
-        self.url = url
-        self.nakala_prefix = kwargs.get("nakala_prefix", "https://api.nakala.fr/")
-        self.can_process = False
-        self.identifier = None
-        self.fileIdentifier = None
-        self.format = None
-        self.type = None
-        self.width = None
-        self.height = None
-        self.extension = None
-
-        self.parse_url()
-
-    def parse_url(self):
-        # Determine if is data source (can process) or embedded (cannot process):
-        urlSplit = []
-        try:
-            urlSplit = self.url.split(self.nakala_prefix)[1].split("data/")[1].split('/')
-            self.can_process = True
-        except:
-            try:
-                urlSplit = self.url.split(self.nakala_prefix)[1].split("embed/")[1].split('/')
-                self.can_process = False
-            except:
-                self.can_process = False
+class NakalaAPI:
+    def __init__(self, **kwargs) -> None:
+        self.nakala_api_prefix = kwargs.get("nakala_api_prefix", "https://api.nakala.fr/")
+        self.nakala_prefix = kwargs.get("nakala_prefix", "https://www.nakala.fr/")
         
-        # If can process, get direct file link:
-        if self.can_process:
-            self.identifier = ""
-            for i in range(len(urlSplit) - 1):
-                self.identifier = self.identifier + "/" + urlSplit[i]
-            
-            data_req = self.data_request()
-            self.fileIdentifier = data_req["files"][0]["sha1"]
-            self.format = data_req["files"][0]["mime_type"]
-            self.type = self.format.split("/")[0].capitalize()
-            self.extension = data_req["files"][0]["extension"]
+    def get_data(self, url):
+        id = self.url_to_id(url)
+        response = requests.get(f"{self.nakala_api_prefix}datas/{id}")
+        return response.json()
+    
+    def search(self, terms, size = 25):
+        term_string = self.get_term_string(terms)
+        response = requests.get(f"{self.nakala_api_prefix}search?q={term_string}&fq=&facet=&order=relevance&page=1&size={str(size)}")
+        return response.json()
 
-            #if self.type == "Image":
-            #    iiif_req = self.iiif_info_request()
-            #    self.width = iiif_req["width"]
-            #    self.height = iiif_req["height"]
-    
-    def data_request(self):
-        r = requests.get(self.nakala_prefix + "datas" + self.identifier)
-        return r.json()
-    
-    def iiif_info_request(self):
-        r = requests.get(self.nakala_prefix + "iiif" + self.identifier + '/' + self.fileIdentifier + "/info.json")
-        return r.json()
+    def get_term_string(self, terms):
+        ret = ""
+        for item in terms:
+            if " " in item:
+                item_split = item.split(" ")
+                for subitem in item_split:
+                    ret = ret + subitem + "%20"
+            else:
+                ret = ret + item + "%20"
+        return ret[:-3]
+
+    def url_to_id(self, url):
+        return url.split(self.nakala_prefix)[1]
+
+
+nakala = NakalaAPI()
+utils.write_json(os.path.join(os.getcwd(), "nakala_out.json"), nakala.get_data("https://www.nakala.fr/10.34847/nkl.def2v5a2"))
+
+search = nakala.search(["carte monde", "iiif"])
+utils.write_json(os.path.join(os.getcwd(), "nakala_search_out.json"), search)
